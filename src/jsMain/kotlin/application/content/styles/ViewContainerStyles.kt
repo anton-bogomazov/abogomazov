6abdf488474d9707f28c93f1ad9bf0ed4747974f
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-01-29 19:19:38 +0300

    Update View Container paddings

--- src/jsMain/kotlin/application/content/styles/ViewContainerStyles.kt
+++ src/jsMain/kotlin/application/content/styles/ViewContainerStyles.kt
@@ -3,12 +3,12 @@ package application.content.styles
 import GlobalStyles
 import org.jetbrains.compose.web.css.*
 
-// todo feels like a bad name
+// todo feels like a bad name for styles
 object ViewContainerStyles : StyleSheet(GlobalStyles) {
 
     val viewContainer by style {
         // todo not sure in percents. maybe its better to to some responsiveness
-        padding(0.px, 25.percent)
+        padding(0.px, 20.percent, 40.px)
     }
 
 }
