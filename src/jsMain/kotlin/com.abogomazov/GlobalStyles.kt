5564dc88e216904bbac5e799727168ebc5a09967
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-02-06 11:16:45 +0300

    feat(style): add google fonts support

--- src/jsMain/kotlin/com.abogomazov/GlobalStyles.kt
+++ src/jsMain/kotlin/com.abogomazov/GlobalStyles.kt
@@ -4,9 +4,22 @@ import org.jetbrains.compose.web.css.*
 
 object GlobalStyles : StyleSheet() {
 
+    val BACKGROUND_COLOR = "#fff"
+    val TEXT_COLOR = "#000"
+    val ACCENT_AND_DIVIDERS_COLOR = "#ddd"
+    val BUTTON_COLOR = "#4D4D4D"
+    val HOVERED_LINK_COLOR = "#808080"
+    val TILE_COLOR = "#4D4D4D"
+
+    val SMALL = 8.px
+    val REGULAR = 12.px
+    val LARGE = 18.px
+    val HUGE = 24.px
+    val GIANT = 36.px
+
     val invertedText by style {
-        backgroundColor(Color.black)
-        color(Color.white)
+        property("background-color", TEXT_COLOR)
+        property("color", BACKGROUND_COLOR)
     }
 
     val flexRow by style {
@@ -19,10 +32,17 @@ object GlobalStyles : StyleSheet() {
         flexDirection(FlexDirection.Column)
     }
 
+    val monospace by style {
+        property("font-family", "Roboto Mono, monospace")
+    }
+
     init {
         universal style {
-            fontFamily("monospace")
-            color(Color.black)
+            property("font-family", "Roboto, sans-serif")
+
+            property("background-color", BACKGROUND_COLOR)
+            property("color", TEXT_COLOR)
+
             textDecoration("none")
 
             type("a:hover") style { color(Color.lightgray) }
