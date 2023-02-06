885a5a8ab2a14a5999a523fc4de59192a1d5b5bb
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-02-06 23:37:23 +0300

    build: add kotlin serialization dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,7 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.6.4")
 
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
             }
         }
         val jsTest by getting {
