39021e195f40b12d7ac90de229c9d69d6d386002
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-02-06 23:39:35 +0300

    refactor: delete unnecessary styles

--- src/jsMain/kotlin/com.abogomazov/application/ViewContainer.kt
+++ src/jsMain/kotlin/com.abogomazov/application/ViewContainer.kt
@@ -1,14 +1,16 @@
 package com.abogomazov.application
 
 import androidx.compose.runtime.Composable
-import com.abogomazov.application.content.style.ViewContainerStyles.viewContainer
-import com.abogomazov.application.routeOnView
+import com.abogomazov.application.content.cv.CvView
+import org.jetbrains.compose.web.css.padding
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Div
 
 @Composable
 fun ViewContainer() {
     Div({
-        classes(viewContainer)
+        style { padding(0.px, 20.percent) }
     }) {
         routeOnView()
     }
