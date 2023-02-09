e637abb24f3c73d8abe910c4bb966275cda5bc4f
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2023-02-09 11:40:25 +0300

    feat(panel): add responsiveness to base layout

--- src/jsMain/kotlin/com.abogomazov/application/ApplicationLayoutStyles.kt
+++ src/jsMain/kotlin/com.abogomazov/application/ApplicationLayoutStyles.kt
@@ -5,20 +5,54 @@ import org.jetbrains.compose.web.css.*
 
 object ApplicationLayoutStyles : StyleSheet(GlobalStyles) {
 
-    private val PANEL_HEIGHT = 100.px
-    private val PANEL_PADDING = 40.px
-    private val VIEW_CONTAINER_PADDING = 20.percent
+    private val PANEL_HEIGHT_SMALL_SCREEN = 50.px
+    private val PANEL_PADDING_SMALL_SCREEN = 20.px
+    private val VIEW_CONTAINER_PADDING_SMALL_SCREEN = 20.px
+
+    private val PANEL_HEIGHT_MEDIUM_SCREEN = 70.px
+    private val PANEL_PADDING_MEDIUM_SCREEN = 30.px
+    private val VIEW_CONTAINER_PADDING_MEDIUM_SCREEN = 80.px
+
+    private val PANEL_HEIGHT_LARGE_SCREEN = 100.px
+    private val PANEL_PADDING_LARGE_SCREEN = 40.px
+    private val VIEW_CONTAINER_PADDING_LARGE_SCREEN = 200.px
 
     val panel by style {
-        height(PANEL_HEIGHT)
-        marginLeft(PANEL_PADDING)
-        marginRight(PANEL_PADDING)
+        height(PANEL_HEIGHT_LARGE_SCREEN)
+        marginLeft(PANEL_PADDING_LARGE_SCREEN)
+        marginRight(PANEL_PADDING_LARGE_SCREEN)
+
+        media(mediaMaxWidth(840.px)) {
+            self style {
+                height(PANEL_HEIGHT_MEDIUM_SCREEN)
+                margin(0.px, PANEL_PADDING_MEDIUM_SCREEN)
+            }
+        }
+
+        media(mediaMaxWidth(420.px)) {
+            self style {
+                height(PANEL_HEIGHT_SMALL_SCREEN)
+                margin(0.px, PANEL_PADDING_SMALL_SCREEN)
+            }
+        }
     }
 
     val viewContainer by style {
-        paddingLeft(VIEW_CONTAINER_PADDING)
-        paddingRight(VIEW_CONTAINER_PADDING)
+        padding(0.px, VIEW_CONTAINER_PADDING_LARGE_SCREEN)
+        marginTop(PANEL_HEIGHT_LARGE_SCREEN)
 
-        marginTop(PANEL_HEIGHT)
+        media(mediaMaxWidth(840.px)) {
+            self style {
+                padding(0.px, VIEW_CONTAINER_PADDING_MEDIUM_SCREEN)
+                marginTop(PANEL_HEIGHT_MEDIUM_SCREEN)
+            }
+        }
+
+        media(mediaMaxWidth(420.px)) {
+            self style {
+                padding(0.px, VIEW_CONTAINER_PADDING_SMALL_SCREEN)
+                marginTop(PANEL_HEIGHT_SMALL_SCREEN)
+            }
+        }
     }
 }
